0cf097b957ff5fac9f364957e374d079d7cb267b
HeytorCardoso (Pessoal) <252641960+HeytorCardoso@users.noreply.github.com>
2026-02-12 10:08:10 -0300

    Final Version PT.02

--- main.py
+++ main.py
@@ -59,4 +59,5 @@ def edit(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host="0.0.0.0", port=port)
